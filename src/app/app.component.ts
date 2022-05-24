9cecf3b5e6296e6735803990c7fbf75d415624f3
Anton Lukashevich <luka14a@gmail.com>
2022-05-24 17:37:33 +0300

    confirm to update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,10 +18,11 @@ export class AppComponent {
         case 'VERSION_READY':
           console.log(`Current app version: ${evt.currentVersion.hash}`);
           console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
-          updates.activateUpdate().then(() => {
-            document.location.reload();
-            console.log(`Current app version: ${evt.currentVersion.hash}`);
-          });
+          if(confirm('Do you want to update APP?')){
+            updates.activateUpdate().then(() => {
+              document.location.reload();
+            });
+          }
           break;
         case 'VERSION_INSTALLATION_FAILED':
           console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);

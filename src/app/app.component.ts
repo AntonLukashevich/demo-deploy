948fafef0f9a99090e28ced01c0c73bd8f9cb627
Anton Lukashevich <luka14a@gmail.com>
2022-05-24 14:37:41 +0300

    swUpdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,19 @@ export class AppComponent {
   title = 'lyrics';
 
   constructor(updates: SwUpdate) {
-    updates.available.subscribe(event => {
-      console.log('current version is', event.current);
-      console.log('available version is', event.available);
-      // @ts-ignore
-      console.log('old version was', event.previous);
-      console.log('new version is', event.current);
+    updates.versionUpdates.subscribe(evt => {
+      switch (evt.type) {
+        case 'VERSION_DETECTED':
+          console.log(`Downloading new app version: ${evt.version.hash}`);
+          break;
+        case 'VERSION_READY':
+          console.log(`Current app version: ${evt.currentVersion.hash}`);
+          console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+          break;
+        case 'VERSION_INSTALLATION_FAILED':
+          console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+          break;
+      }
     });
   }
 
